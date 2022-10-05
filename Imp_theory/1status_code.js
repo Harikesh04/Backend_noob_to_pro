83aaf5fbab2c3c29988529414e93cb0271d3d4d7
Harry <harikeshkrsingh20@gamail.com>
2022-10-05 10:12:40 +0530

    initial commit

---
+++ Imp_theory/1status_code.js
@@ -0,0 +1,15 @@
+//status code: short message from server to the client
+// req--->
+//        <-----res
+
+//this res comes with a status code so that we will know what had happened behind the scenes
+//helps in debuging 
+
+// status codes are divided into cat/class
+1.100-199 //information purpose
+2.200-299 //success
+3.300-399// redirect
+4.400-499//client err
+5.500 -599//server fault eg server down,server crash
+
+//link- https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
